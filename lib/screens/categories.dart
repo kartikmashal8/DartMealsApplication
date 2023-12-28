6e4a132b445b19378dfb44652329f9659adcea1e
Kartik Mashal <kartikmashal@Kartiks-MacBook-Pro.local>
2023-12-28 15:55:50 +0530

    commit on meals app

---
+++ lib/screens/categories.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen(
+      {super.key,
+      required this.onToggleFavorite,
+      required this.availableMeals});
+  final void Function(Meal meal) onToggleFavorite;
+  final List<Meal> availableMeals;
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = availableMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+          onToggleFavorite: onToggleFavorite,
+        ),
+      ),
+    ); //Navigator.push(context, Route);
+  }
+
+  @override
+  Widget build(context) {
+    return GridView(
+      padding: const EdgeInsets.all(16),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
+      children: [
+        for (final category in availableCategories) //   -- Method 1
+          CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              })
+        // ...availableCategories                        -- Method 2
+        //     .map((category) => CategoryGridItem(category: category))
+        // ...availableCategories                        -- Method 3
+        //     .map((category) => CategoryGridItem(category: category))
+        //     .toList()
+      ],
+    );
+  }
+}
